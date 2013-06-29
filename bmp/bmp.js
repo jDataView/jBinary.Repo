e05e70905fa7e036c4c510be4f960259e0518af4
Ingvar Reverser <me@rreverser.com>
2013-06-29 22:18:01 +0300

    Small code style improvements.

--- bmp/bmp.js
+++ bmp/bmp.js
@@ -1,39 +1,42 @@
 jBinary.Repo.BMP = {
-	bgr: {
+	BGR: {
 		b: 'uint8',
 		g: 'uint8',
 		r: 'uint8'
 	},
-	bgra: ['extend', 'bgr', {
+
+	BGRA: ['extend', 'BGR', {
 		a: 'uint8'
 	}],
-	size: {
+
+	Dimensions: {
 		horz: 'uint32',
 		vert: 'uint32'
 	},
-	header: {
+
+	Header: {
 		// bitmap "magic" signature
 		_signature: ['const', ['string', 2], 'BM', true],
-		// full file size
+		// full file Dimensions
 		fileSize: 'uint32',
 		// reserved
 		reserved: 'uint32',
 		// offset of bitmap data
 		dataOffset: 'uint32',
-		// size of DIB header
+		// Dimensions of DIB header
 		dibHeaderSize: 'uint32',
 		// image dimensions
-		size: 'size',
+		size: 'Dimensions',
 		// color planes count (equals 1)
 		planesCount: 'uint16',
 		// color depth (bits per pixel)
 		bpp: 'uint16',
 		// compression type
 		compression: 'uint32',
-		// size of bitmap data
+		// Dimensions of bitmap data
 		dataSize: 'uint32',
 		// resolutions (pixels per meter)
-		resolution: 'size',
+		resolution: 'Dimensions',
 		// total color count
 		colorsCount: jBinary.Template({
 			baseType: 'uint32',
@@ -49,7 +52,7 @@ jBinary.Repo.BMP = {
 			}
 		}),
 		// color palette (mandatory for <=8bpp images)
-		palette: ['if', function (context) { return context.bpp <= 8 }, ['array', ['extend', 'bgr', {_padding: ['skip', 1]}], 'colorsCount']],
+		palette: ['if', function (context) { return context.bpp <= 8 }, ['array', ['extend', 'BGR', {_padding: ['skip', 1]}], 'colorsCount']],
 		// color masks (needed for 16bpp images)
 		mask: {
 			r: 'uint32',
--- tar/tar.js
+++ tar/tar.js
@@ -23,69 +23,69 @@ jBinary.Repo.TAR = {
 	}],
 
 	FileItem: ['extend',
-		{
-			name: ['string0', 100],
-			mode: 'Oct8',
-			owner: 'Oct8',
-			group: 'Oct8',
-			size: jBinary.Template({
-				baseType: 'Oct12',
-				write: function (context) {
-					this.baseWrite(context.content_binary.view.byteLength);
-				}
-			}),
-			mod_time: jBinary.Template({
-				baseType: 'Oct12',
-				read: function () {
-					return new Date(this.baseRead() * 1000);
-				},
-				write: function (dateTime) {
-					this.baseWrite(dateTime / 1000);
-				}
-			}),
-			checksum: 'Oct8',
-			type: ['enum', 'char', {
-				'0': 'file',
-				'1': 'hard',
-				'2': 'symbolic',
-				'3': 'device_char',
-				'4': 'device_block',
-				'5': 'directory',
-				'6': 'pipe',
-				'7': 'contiguous'
-			}],
-			name_linked: ['string0', 100],
-			_ustar: ['const', ['string', 5], 'ustar']
-		},
-		['if', function (context) { return context._ustar === undefined || context._ustar === 'ustar' }, {
-			_ustar_finish: ['const', 'char', '\0'],
-			_ustar_version: ['const', ['string', 2], '00'],
-			owner_name: ['string0', 32],
-			group_name: ['string0', 32],
-			device: ['array', 'Oct8', 2],
-			name_prefix: ['string0', 155]
+	{
+		name: ['string0', 100],
+		mode: 'Oct8',
+		owner: 'Oct8',
+		group: 'Oct8',
+		size: jBinary.Template({
+			baseType: 'Oct12',
+			write: function (context) {
+				this.baseWrite(context.content_binary.view.byteLength);
+			}
+		}),
+		mod_time: jBinary.Template({
+			baseType: 'Oct12',
+			read: function () {
+				return new Date(this.baseRead() * 1000);
+			},
+			write: function (dateTime) {
+				this.baseWrite(dateTime / 1000);
+			}
+		}),
+		checksum: 'Oct8',
+		type: ['enum', 'char', {
+			'0': 'file',
+			'1': 'hard',
+			'2': 'symbolic',
+			'3': 'device_char',
+			'4': 'device_block',
+			'5': 'directory',
+			'6': 'pipe',
+			'7': 'contiguous'
 		}],
-		{
-			_padding1: 'Padding',
-			content_binary: jBinary.Type({
-				read: function () {
-					var header = this.binary.getContext(1);
-					if (!header.size) {
-						return;
-					}
-					var startPos = this.binary.tell();
-					this.binary.skip(header.size);
-					return this.binary.slice(startPos, this.binary.tell());
-				},
-				write: function (binary) {
-					if (!binary) {
-						return;
-					}
-					this.binary.write('blob', binary.read('blob', 0));
+		name_linked: ['string0', 100],
+		_ustar: ['const', ['string', 5], 'ustar']
+	},
+	['if', function (context) { return context._ustar === undefined || context._ustar === 'ustar' }, {
+		_ustar_finish: ['const', 'char', '\0'],
+		_ustar_version: ['const', ['string', 2], '00'],
+		owner_name: ['string0', 32],
+		group_name: ['string0', 32],
+		device: ['array', 'Oct8', 2],
+		name_prefix: ['string0', 155]
+	}],
+	{
+		_padding1: 'Padding',
+		content_binary: jBinary.Type({
+			read: function () {
+				var header = this.binary.getContext(1);
+				if (!header.size) {
+					return;
 				}
-			}),
-			_padding2: 'Padding'
-		}
+				var startPos = this.binary.tell();
+				this.binary.skip(header.size);
+				return this.binary.slice(startPos, this.binary.tell());
+			},
+			write: function (binary) {
+				if (!binary) {
+					return;
+				}
+				this.binary.write('blob', binary.read('blob', 0));
+			}
+		}),
+		_padding2: 'Padding'
+	}
 	],
 
 	File: jBinary.Template({
